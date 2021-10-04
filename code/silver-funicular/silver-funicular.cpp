dd7f134c96457aa662653fa77990f50fa12fa2d7
Eldar <35378787+eldarbr@users.noreply.github.com>
2021-10-04 18:35:15 +0300

    Update silver-funicular.cpp

--- code/silver-funicular/silver-funicular.cpp
+++ code/silver-funicular/silver-funicular.cpp
@@ -2,26 +2,25 @@
 using namespace std;
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    //Входной контроль
-    float Eps;
-    cout << "Введите Eps: ";
-    cin >> Eps;
-    if (Eps <= 0) {
-        cout << "ОШИБКА! Eps должен быть больше 0!";
-        exit;
-    }
-    //Инициализация переменных
-    double Sum = 0;
-    int N = 1;
-    //Рассчет суммы
-    Sum += 1 / (N * (++N));
-    while ((1 - Sum) < Eps) {
-        Sum += 1 / (N * (++N));
-    }
-    //Вывод результатов
-    cout << "Количество просумированных элементов N = " << N << endl;
-    cout << "Сумма Sum = " << Sum << endl;
-    cout << "Разность суммы и точного значения (1 - Sum) = " << (1 - Sum) << endl;
-    return 0;
+	setlocale(LC_ALL, "Russian");
+	//Входной контроль
+	float Eps;
+	cout << "Введите Eps: ";
+	cin >> Eps;
+	if (Eps <= 0) {
+		cout << "ОШИБКА! Eps должен быть больше 0!";
+		return 1;
+	}
+	//Инициализация переменных
+	double Sum = 0;
+	int N = 1;
+	//Рассчет суммы
+	do {
+		Sum += 1.0 / (N * (++N));
+	} while ((1 - Sum) > Eps);
+	//Вывод результатов
+	cout << "Количество просумированных элементов N = " << N << endl;
+	cout << "Сумма Sum = " << Sum << endl;
+	cout << "Разность суммы и точного значения (1 - Sum) = " << (1 - Sum) << endl;
+	return 1;
 }
