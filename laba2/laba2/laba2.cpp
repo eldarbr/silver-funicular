bc7c38470da52beb2cd246f8b1ea8fd0a5b18c71
NailN03 <nurievnail03@mail.ru>
2021-11-14 15:33:58 +0300

    update laba2

--- laba2/laba2/laba2.cpp
+++ laba2/laba2/laba2.cpp
@@ -1,8 +1,48 @@
 ﻿#include <iostream>
 #include <cmath>
-#include "laba2.h"
+#include <iomanip>
 
 using namespace std;
+
+void HeadTable() {
+    cout << "\t"
+        << char(218) << setfill(char(196)) << setw(20)
+        << char(194) << setfill(char(196)) << setw(20)
+        << char(194) << setfill(char(196)) << setw(20)
+        << char(194) << setfill(char(196)) << setw(20)
+        << char(191) << endl;
+    cout << "\t"
+        << char(179) << setw(19) << "#" << setfill(' ')
+        << char(179) << setw(19) << "x" << setfill(' ')
+        << char(179) << setw(19) << "F(x)" << setfill(' ')
+        << char(179) << setw(19) << "G(x)" << setfill(' ')
+        << char(179) << endl;
+    cout << "\t"
+        << char(195) << setfill(char(196)) << setw(20)
+        << char(197) << setfill(char(196)) << setw(20)
+        << char(197) << setw(20)
+        << char(197) << setw(20) << char(180) << endl;
+    cout.setf(ios::showpoint | ios::fixed);
+
+}
+
+void BottomTable() {
+    cout << "\t"
+        << char(192) << setfill(char(196)) << setw(20)
+        << char(193) << setfill(char(196)) << setw(20)
+        << char(193) << setfill(char(196)) << setw(20)
+        << char(193) << setfill(char(196)) << setw(20)
+        << char(217) << endl;
+}
+
+void  BetweenTheRaw() {
+    cout << "\t"
+        << char(195) << setfill(char(196)) << setw(20)
+        << char(197) << setfill(char(196)) << setw(20)
+        << char(197) << setfill(char(196)) << setw(20)
+        << char(197) << setfill(char(196)) << setw(20)
+        << char(180) << endl;
+}
 
 double F(double x) {
     return pow(x - 1, 3);
@@ -15,6 +55,7 @@ double G(double x) {
 int main()
 {
     setlocale(LC_ALL, "Russian");
+    system("color F0");
 
     //Инициализация переменных
     double A;
@@ -31,14 +72,24 @@ int main()
     cin >> B;
     cout << "Введите N - количество интервалов, на которые разделен отрезок [A;B]: \t";
     cin >> N;
-
+    setlocale(LC_ALL, "C");
     h = (B - A) / N; //Рассчет величины шага
-
+    HeadTable();
     for (i = 0; i <= N; i++) {
         x = A + i * h;
-        cout << x << "\t" << F(x) << "\t" << G(x) << endl;
+        //cout << x << "\t" << F(x) << "\t" << G(x) << endl;
+        
+        cout << "\t" << char(179) << setw(19) << setfill(char(255)) << i
+            << char(179) << setw(19) << setprecision(4) << x
+            << setfill(char(255))
+            << char(179) << setw(19) << setprecision(4) << F(x)
+            << setfill(char(255))
+            << char(179) << setw(19) << setprecision(4) << G(x)
+            << setfill(char(255)) << char(179) << endl;
+        if (i!=N)
+            BetweenTheRaw();
     }
-
+    BottomTable();
     return 0;
 }
 
