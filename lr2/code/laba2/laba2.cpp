aa20d5823c0b3b9afb756c77d9b54bae32a5ef11
Eldar <35378787+eldarbr@users.noreply.github.com>
2021-11-16 13:33:54 +0300

    Update laba2.cpp

--- lr2/code/laba2/laba2.cpp
+++ lr2/code/laba2/laba2.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+
+// псевдографика - заголовок таблицы
 void HeadTable() {
     cout << "\t"
         << char(218) << setfill(char(196)) << setw(20)
@@ -26,6 +28,7 @@ void HeadTable() {
 
 }
 
+// псевдографика - завершение таблицы
 void BottomTable() {
     cout << "\t"
         << char(192) << setfill(char(196)) << setw(20)
@@ -35,6 +38,7 @@ void BottomTable() {
         << char(217) << endl;
 }
 
+// псевдографика - тело таблицы
 void  BetweenTheRaws() {
     cout << "\t"
         << char(195) << setfill(char(196)) << setw(20)
@@ -44,13 +48,36 @@ void  BetweenTheRaws() {
         << char(180) << endl;
 }
 
+// расчет значения функции F(x) в точке x
 double F(double x) {
     return pow(x - 1, 3);
 }
 
+// расчет значения функции G(x) в точке x
 double G(double x) {
     return pow(x + 5, 3) / (1 + pow(sin(x), 2));
 }
+
+// вывод строк таблицы с автоматическим закрытием таблицы
+void PrintTable(double x, int i, int N, bool debug = false) {
+    if (debug) {
+        cout << x << "\t" << F(x) << "\t" << G(x) << endl;
+    }
+    else {
+        cout << "\t" << char(179) << setw(19) << setfill(char(255)) << i
+            << char(179) << setw(19) << setprecision(4) << x
+            << setfill(char(255))
+            << char(179) << setw(19) << setprecision(4) << F(x)
+            << setfill(char(255))
+            << char(179) << setw(19) << setprecision(4) << G(x)
+            << setfill(char(255)) << char(179) << endl;
+        if (i != N)
+            BetweenTheRaws();
+        else
+            BottomTable();
+    }
+}
+
 
 int main()
 {
@@ -64,12 +91,14 @@ int main()
     double h;
     double x;
     int i;
+    bool debug = false;
 
-    //Ввод входных данных
+    // Ввод и проверка данных
     cout << "Введите А - координату начала отрезка [A;B]: \t";
     cin >> A;
     cout << "Введите B - координату конца отрезка [A;B]: \t";
     cin >> B;
+
     if (A > B) {
         cout << "\nОШИБКА! А должен быть меньше или равен В!";
         return 1;
@@ -79,29 +108,17 @@ int main()
     if (N <= 0) {
         cout << "\nОШИБКА! N должно быть больше 0! ";
         return 1;
-    } 
-    else if(N % 1 != 0) {
-        cout << "\nОШИБКА! N должно быть целым числом!";
-        return 1;
     }
+
+    // Подготовка таблицы
     setlocale(LC_ALL, "C");
+    if (!debug) HeadTable();
+
     h = (B - A) / N; //Рассчет величины шага
-    HeadTable();
+
     for (i = 0; i <= N; i++) {
         x = A + i * h;
-        //cout << x << "\t" << F(x) << "\t" << G(x) << endl;
-        
-        cout << "\t" << char(179) << setw(19) << setfill(char(255)) << i
-            << char(179) << setw(19) << setprecision(4) << x
-            << setfill(char(255))
-            << char(179) << setw(19) << setprecision(4) << F(x)
-            << setfill(char(255))
-            << char(179) << setw(19) << setprecision(4) << G(x)
-            << setfill(char(255)) << char(179) << endl;
-        if (i!=N)
-            BetweenTheRaws();
-        else
-            BottomTable();
+        PrintTable(x, i, N, debug);
     }
     return 0;
 }
