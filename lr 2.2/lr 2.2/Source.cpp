afb9c6714254dbc0a45e4b95aaffa43196e8eeac
NailN03 <nurievnail03@mail.ru>
2022-04-06 08:52:27 +0300

    Haven’t I seen you some place before?

--- lr 2.2/lr 2.2/Source.cpp
+++ lr 2.2/lr 2.2/Source.cpp
@@ -9,9 +9,6 @@ void Errors(int x) {
     case 1:
         cout << "Ошибка! Файл не найден! " << endl;
         exit(0);
-    case 2:
-        cout << "Ошибка! Файл пустой! " << endl;
-        exit(0);
     case 3:
         cout << "Ошибка! Количество строк должно быть больше нуля! " << endl;
         exit(0);
@@ -25,7 +22,7 @@ void Errors(int x) {
 }
 
 // ФУНКЦИЯ ДЛЯ НАХОЖДЕНИЯ МАКСИМУМА
-int maxim(int ber, int ike) {
+long long maxim(long long ber, long long ike) {
     if (ber > ike)
         return ber;
     else
@@ -59,8 +56,8 @@ void print_matrix(int** arr, int row, int col) {
 
 // ФУНКЦИЯ ДЛЯ ННАХОЖДЕНИЯ ПРОИЗВЕДЕНИЙ СТРОК МАТРИЦЫ ПО ЗАДАНИЮ
 void product(int** arr, int row, int col) {
-    int prod = 1;
-    int max_prod = 1;
+    long long prod = 1;
+    long long max_prod = 1;
     int sign = 0;
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
@@ -81,7 +78,12 @@ void product(int** arr, int row, int col) {
     cout << "Максимальное произведение элементов строки, не содержащей ноль:    " << max_prod << endl << endl;
 }
 
-const string filename = "file.txt";
+//const string filename = "file.txt";
+//const string filename = "test1.txt";
+//const string filename = "test2.txt";
+//const string filename = "err_test1.txt";
+//const string filename = "err_test2.txt";
+//const string filename = "stealth_file.txt";
 
 int main()
 {
@@ -89,11 +91,9 @@ int main()
     system("color F0");
     int N, M;
     int fl;
-    ifstream fn;
-    fn.open(filename);
     cout << "Введите \"0\", если хотите ввести данные с руки,  \"1\", если хотите считать их из файла" << endl;
     cin >> fl;
-
+    ifstream fn;
     if (fl == 0) {
         cout << "Введите количество строк:  ";
         cin >> N;
@@ -106,11 +106,10 @@ int main()
         cout << endl;
     }
     else if (fl == 1) {
+        fn.open(filename);
         if (!fn.is_open())
             Errors(1);
-        if (fn.eof())
-            Errors(2);
-        fn >> N >> M;
+        fn >> N;
         if (N == 0)
             Errors(3);
         if (M == 0)
