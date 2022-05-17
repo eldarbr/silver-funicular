523bf31e0a15259c838b44b6b5c54d30a9b9c09b
NailN03 <nurievnail03@mail.ru>
2022-05-17 21:39:04 +0300

    hayo

--- lr2.3/lr2.2/lr2.2.cpp
+++ lr2.3/lr2.2/lr2.2.cpp
@@ -52,16 +52,18 @@ void BetweenTheRaws() {
         << char(180) << endl;
 }
 
-// вывод строк таблицы с автоматическим закрытием таблицы
+// вывод строк таблицы с закрытием таблицы
 void PrintTable(int n, char m[], char k[], int kol, double gruz, bool last) {
-    cout << "\t" << char(179) << setw(19) << setfill(char(255)) << n
-        << char(179) << setw(19) << setprecision(4) << m
+    cout << "\t"
+        << char(179) << setw(19) << setfill(char(255)) << n
+        << setfill(char(255))
+        << char(179) << setw(19) << setprecision(0) << m
         << setfill(char(255))
         << char(179) << setw(19) << setprecision(4) << k
         << setfill(char(255))
         << char(179) << setw(19) << setprecision(4) << kol
         << setfill(char(255))
-        << char(179) << setw(19) << setprecision(4) << gruz
+        << char(179) << setw(19) << setprecision(1) << gruz
         << setfill(char(255)) << char(179) << endl;
     if (last) {
         BottomTable();
@@ -73,7 +75,7 @@ void PrintTable(int n, char m[], char k[], int kol, double gruz, bool last) {
 
 // структура
 struct aero {
-    char m[7];
+    char m[8];
     char n[6];
     int kol;
     double gruz;
@@ -93,47 +95,53 @@ void str_from_file(const string file, aero* mas) {
     }
 }
 
-// бортовой номер
+// считывание бортовой номер
 int num_from_struct(aero mas) {
     int number = atoi(&mas.n[2]);
     return number;
 }
 
-// сортировка структуры пузырьком
-void sort_the_struct(aero* mas, int M) {
-    aero Temp;
-    int exch;
-    for (int i = 0; i < M-1; i++) {
-        for (int j = M-1; j > i; j--){
-            if (num_from_struct(mas[j]) < num_from_struct(mas[j - 1])) {
-                Temp = mas[j];
-                mas[j] = mas[j - 1];
-                mas[j - 1] = Temp;
+// сортировка структуры пузырьком по индексам
+void sort_the_struct(aero* mas, int M, int *rar) {
+    int Temp;
+    for (int i = 0; i < M - 1; i++) {
+        for (int j = M - 1; j > i; j--) {
+            if (num_from_struct(mas[rar[j]]) < num_from_struct(mas[rar[j-1]])) {
+                Temp = rar[j];
+                rar[j] = rar[j - 1];
+                rar[j - 1] = Temp;
             }
         }
     }
 }
-
 const string filename = "file.txt";
 
 int main()
 {
-    int pas = 0;
-    double allgruz = 0;
-    HeadTable();
-    ifstream fn;
-    fn.open(filename);
-    int N;
-    fn >> N;
+    int pas = 0;            // количество пассажиров
+    double allgruz = 0;     // масса груза
+    HeadTable();            // печать оглавления таблицы
+    ifstream fn;            // создание переменной класса ifstream
+    fn.open(filename);      // открытие файла
+    int N;                  // количество строк данных в файле
+    fn >> N;                // чтение количества строк данных в файле
+    int* rar = new int[N];  // массив индексов для сортировки по индексам 
+    for (int i = 0; i < N; i++)
+        rar[i] = i;         // заполенение массива индексов
     aero* arr = new aero[N];
     str_from_file(filename, arr);
-    bool last = 0;
-    sort_the_struct(arr, N);
+    bool last = 0;          // флаг конца таблицы
+    sort_the_struct(arr, N, rar);   // сортировка структуры по индексам
     for (int i = 0; i < N; i++) {
-        pas += arr[i].kol;
-        allgruz += arr[i].gruz;
-        PrintTable(i + 1, arr[i].m, arr[i].n, arr[i].kol, arr[i].gruz, last);
+        int k;
+        k = rar[i];
+        pas += arr[k].kol;
+        allgruz += arr[k].gruz;
+        PrintTable(i + 1, arr[k].m, arr[k].n, arr[k].kol, arr[k].gruz, last);
         if (i == N - 2)
-            last = 1;
+            last = 1;        
     }
+    setlocale(LC_ALL, "rus");
+    cout << "\n\tСуммарное количество пассажиров: " << pas << "\n\tСуммарный вес груза: " << setprecision(1) << allgruz << "\n\n";
+    system("pause");
 }
