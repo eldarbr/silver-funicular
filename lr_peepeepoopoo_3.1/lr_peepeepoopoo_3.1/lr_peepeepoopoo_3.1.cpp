8430635bf2bfdbc88d0abf7c06ac6bceec9d5ba0
NailN03 <nurievnail03@mail.ru>
2022-09-25 14:42:44 +0300

    update 25.09.2222

--- lr_peepeepoopoo_3.1/lr_peepeepoopoo_3.1/lr_peepeepoopoo_3.1.cpp
+++ lr_peepeepoopoo_3.1/lr_peepeepoopoo_3.1/lr_peepeepoopoo_3.1.cpp
@@ -63,17 +63,68 @@ void generate_random(float* arr, unsigned int length) {
 	}
 }
 
+void generate_sawtooth(float* arr, unsigned int length, float min, float max, float interval) {
+	float step = (max - min) / interval;
+	int num = 1;
+	*(arr) = min;
+	for (int i = 1; i < length; i++) {
+		*(arr + i) = *(arr + i - 1) + step;
+		num++;
+		if (num == interval) {
+			i++;
+			*(arr + i) = min;
+			num = 1;
+		}		
+	}
+}
+
+void generate_sinuous(float* arr, unsigned int length, float min, float max, float interval) {
+	float mid = (max + min) / 2;
+	float step = mid / interval;
+	int k = 0;
+	*(arr) = mid;
+	for (int i = 1; i < length; i++) {
+		*(arr + i) = *(arr + i - 1) + pow(-1, k) * step;
+		if (arr[i]+ pow(-1, k) * step < min || arr[i]+ pow(-1, k) * step > max)
+			k++;
+	}
+}
+
+void generate_stepped(float* arr, unsigned int length, float min, float max, float interval) {
+	float step = (max - min) / interval;
+	*(arr) = min;
+	int k = 0;
+	int num = 1;
+	for (int i = 1; i < length; i++) {
+		*(arr + i) = *(arr + k) + random()/10000;
+		num++;
+		if (num == interval) {
+			num = 1;
+			k = i + 1;
+			*(arr + k) = *(arr + i) + step;
+		}
+	}
+}
+	
 
 int main()
 {
 	srand(time(NULL));
-
+	setlocale(LC_ALL, "rus");
 	int length;
+	int min;
+	int max;
+	int interval;
 	cout << "BBE4UTE 4/\\UNY MACCUBA: ";
 	cin >> length;
 	float* arr = new float[length];
-
-	generate_descending(arr, length);
+	cout << "ВВВЕДИТЕ НИЖНЮЮ ГРАНИЦУ: ";
+	cin >> min;
+	cout << "ВВЕДИТЕ ВЕРХНЮЮ ГРАНИЦУ: ";
+	cin >> max;
+	cout << "ВВЕДИТЕ ИНТЕРВАЛ: ";
+	cin >> interval;
+	generate_stepped(arr, length, min, max, interval);
 	for (int i = 0; i < length; i++) cout << arr[i] << "\n";
 
 	return 0;
