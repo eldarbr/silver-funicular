efe2e3529e74ed48020b8c7d43ff008031d05926
NailN03 <nurievnail03@mail.ru>
2022-10-11 09:49:55 +0300

    Bitch, я аристократичный папочка

---
+++ lr_ladidie_3.3/lr_ladidie_3.3/lr_ladidie_3.3/lr_ladidie_3.3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void ShellSort();
+void ChooseSort();
+
+
+void swap(int* arr, int i) {
+	int temp;
+	temp = arr[i];
+	arr[i] = arr[i - 1];
+	arr[i - 1] = temp;
+}
+
+int main()
+{
+	
+}
