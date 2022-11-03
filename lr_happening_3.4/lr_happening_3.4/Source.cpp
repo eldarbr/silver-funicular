7ed86b6bdfab3adda9b91ab0da402f7bc0624143
NailN03 <nurievnail03@mail.ru>
2022-11-04 00:51:22 +0300

    parasha

---
+++ lr_happening_3.4/lr_happening_3.4/Source.cpp
@@ -0,0 +1,29 @@
+// Однонаправленный кольцевой целочисленный список
+
+#include <iostream>
+
+using namespace std;
+
+const int n = 5;
+
+struct elem { int data; elem* next; };
+
+void searchInList() {
+
+}
+
+void recordIntoList(elem*begin, elem*last, elem*list, int a[]) {
+	for (int i = 0; i < n; i++) {
+		last = new elem;
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	int A[n] = { 1,4,6,8,9 };
+	elem* begin = NULL;
+	elem* last = NULL;
+	elem* list = NULL;
+	
+	
+}
