be60c5371cfbb882d243c14af718a8c1f857e690
NailN03 <nurievnail03@mail.ru>
2022-11-08 10:43:54 +0300

    flk

--- lr_happening_3.4/lr_happening_3.4/Source.cpp
+++ lr_happening_3.4/lr_happening_3.4/Source.cpp
@@ -6,34 +6,61 @@ using namespace std;
 
 const int n = 5;
 
-struct elem { int data; elem* next; };
 
-struct List { elem* begin; elem* end; };
+struct Element { int data; Element* next; };
+
 
 void searchInList() {
 
 }
 
-void recordIntoList(List* list, int *a[], int n) {
-	List *ptr = new List;
-	if (!list->begin)
-		list->begin = ptr;
-	else
-		list->end->next = 
-
+void recordIntoList(int a[], int n, Element* begin, Element* last) {
+	
+	int i = 0;
+	while (n > 0) {
+		if (!begin) {
+			Element* elem = new Element;
+			elem->data = a[i];
+			i++;
+			elem->next = NULL;
+			begin = last = elem;
+		}
+		else {
+			Element* elem = new Element;
+			elem->data = a[i];
+			i++;
+			last->next = elem;
+			last = elem;
+		}
+		n--;
+	}
+	last->next = begin;
 }
+
+char redBold[] = "\x1b[31;1m";
+char normal[] = "\x1b[39;49m";
+
+void printMenu() {
+	system("cls");
+	cout << redBold << "Что вы хотите сделать?\n";
+	cout << "1. Перейти на следующий элемент в списке\n";
+	cout << "2. Выйти из программы\n";
+	cout << ">" << normal;
+}
+
+
 
 int main() {
 	setlocale(LC_ALL, "rus");
-	int n;
+	int A[n] = { 5,2,7,8,1 };
+	/*int n;
 	cout << "Введите n: ";
 	cin >> n;
 	cout << endl << "Введите массив: ";
 	int* arr = new int[n];
-	cout << endl;
-	elem* begin = NULL;
-	elem* last = NULL;
-	elem* list = NULL;
+	cout << endl;*/
+	Element* begin = NULL;
+	Element* last = NULL;
 	
-	
+	printMenu();
 }
