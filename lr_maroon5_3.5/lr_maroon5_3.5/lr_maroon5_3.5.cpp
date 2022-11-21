71476193928052da49d0f6891314082ea1bf8c24
NailN03 <nurievnail03@mail.ru>
2022-11-21 13:09:38 +0300

    lr5

---
+++ lr_maroon5_3.5/lr_maroon5_3.5/lr_maroon5_3.5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+const int n = 5;
+
+struct Queue {
+    int data;
+    int index;
+    int priority;
+    Queue* next;
+};
+
+struct Queue* front = NULL;
+struct Queue* rear = NULL;
+int k;
+
+void Add(int number, int priority, int max) {
+
+}
+
+int main()
+{
+    
+}
