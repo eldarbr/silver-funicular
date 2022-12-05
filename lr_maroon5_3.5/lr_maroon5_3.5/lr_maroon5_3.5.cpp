c0c9bc065a6b5165988865cea1d749bef3c82526
Eldar <35378787+eldarbr@users.noreply.github.com>
2022-12-05 14:48:28 +0300

    Final fifth

--- lr_maroon5_3.5/lr_maroon5_3.5/lr_maroon5_3.5.cpp
+++ lr_maroon5_3.5/lr_maroon5_3.5/lr_maroon5_3.5.cpp
@@ -32,7 +32,7 @@ Queue* CreateQueue(Element element, Queue* trailing) {
 Queue* Enqueue(Queue* queue, Element element) {
 	// disallow insertion of elements with same priority value
 	if (element.priorityValue == queue->element.priorityValue) {
-		throw new exception("Elements with the same priority not allowed");
+		throw "Elements with the same priority not allowed";
 	}
 	else if (element.priorityValue > queue->element.priorityValue) {
 		return CreateQueue(element, queue);
@@ -50,32 +50,67 @@ Queue* Enqueue(Queue* queue, Element element) {
 }
 
 /// <summary>
-/// Locate and return the element with the lowest priority in the queue
+/// Locate and return element with the lowest priority in the queue
 /// </summary>
-Element LowestPriorityElement(Queue* queue) {
+Element FindLowestPriorityElement(Queue* queue) {
 	if (queue->isLast) {
 		return queue->element;
 	}
 	else {
-		return LowestPriorityElement(queue->nextElement);
+		return FindLowestPriorityElement(queue->nextElement);
+	}
+}
+
+/// <summary>
+/// Locate and delete element with the lowest priority in the queue
+/// </summary>
+Queue* ExcludeLowestPriorityElement(Queue* queue) {
+	if (queue->isLast) {
+		delete queue;
+		return NULL;
+	}
+	else if (queue->nextElement->isLast) {
+		delete queue->nextElement;
+		queue->nextElement = NULL;
+		queue->isLast = true;
+		return queue;
+	}
+	else {
+		queue->nextElement = ExcludeLowestPriorityElement(queue->nextElement);
+		return queue;
 	}
 }
 
 /// <summary>
 /// Clear the queue
 /// </summary>
-void DeleteQueue(Queue* queue) {
+Queue* DeleteQueue(Queue* queue) {
+	if (queue->isLast) {
+		delete queue;
+		return NULL;
+	}
 	if (!queue->nextElement->isLast) {
 		DeleteQueue(queue->nextElement);
 	}
 	delete queue->nextElement;
+	return NULL;
 }
 
 class ConsoleInterface {
 public:
+	enum ConsoleInputBBB { Creating, Acting };
+
 	static void OutputQueue(Queue* queue) {
-		Queue* copy = queue;
 		cout << "\nTable of content of the queue\n";
+		Queue* copy = queue;
+		cout << "indices:\t";
+		int i = 0;
+		while (copy != NULL) {
+			cout << i++ << "\t";
+			copy = copy->nextElement;
+		}
+		copy = queue;
+		cout << "\n";
 		cout << "data value:\t";
 		while (copy != NULL) {
 			cout << copy->element.dataValue << "\t";
@@ -90,6 +125,113 @@ public:
 		cout << "\n";
 	}
 
+	static void MainDialogue() {
+		setlocale(LC_ALL, "rus");
+		Queue* myQueue = NULL;
+		int variant;
+		int index;
+		do {
+			if (myQueue != NULL) {		// список задан
+				system("cls");
+				ConsoleInterface::OutputQueue(myQueue);
+				cout
+					<< "\nWhat's the matter?\n"
+					<< "1. Add new element to the queue\n"
+					<< "2. Find element with the lowest priority\n"
+					<< "3. Find and exclude from the queue element with the lowest priority\n"
+					<< "4. Delete whole queue\n"
+					<< "5. Exit\n"
+					<< "> ";
+				variant = ConsoleInterfaceInput(Acting);
+				switch (variant) {
+				case 1:
+					cout << "\nInput one pair of integers (value - priority)\n> ";
+					int val, pr;
+					cin >> val >> pr;
+
+					try {
+						myQueue = Enqueue(myQueue, Element{ val,pr });
+					}
+					catch (const char* msg) {
+						cout << msg << endl;
+						system("pause");
+					}
+					break;
+				case 2:
+					Element lowestPcasetwo = FindLowestPriorityElement(myQueue);
+					cout << "\nFound element with the lowest priority:\n"
+						<< "Value:\t\t" << lowestPcasetwo.dataValue << endl
+						<< "Priority:\t" << lowestPcasetwo.priorityValue << endl << endl;
+					system("pause");
+					break;
+				case 3:
+					Element lowestPcasethree = FindLowestPriorityElement(myQueue);
+					myQueue = ExcludeLowestPriorityElement(myQueue);
+					cout << "\nFound element with the lowest priority:\n"
+						<< "Value:\t\t" << lowestPcasethree.dataValue << endl
+						<< "Priority:\t" << lowestPcasethree.priorityValue << endl << endl;
+					cout << "New queue\n";
+					ConsoleInterface::OutputQueue(myQueue);
+					system("pause");
+					break;
+				case 4:
+					myQueue = DeleteQueue(myQueue);
+					break;
+				}
+			}
+			else {					// список не задан
+				system("cls");
+				cout << "\tWhat's the matter?\n"
+					<< "1. Create new queue\n"
+					<< "2. Exit\n"
+					<< "> ";
+				variant = ConsoleInterfaceInput(Creating);
+				switch (variant)
+				{
+				case 1:
+					int n;
+					cout << "\nWhat's the lenght of the queue: \n> ";
+					cin >> n;
+					if (n <= 0) {
+						break;
+					}
+					int* arr = new int[2 * n];
+					cout << "\nInput " << n << " pairs of integers (value - priority)\n> ";
+					for (int i = 0; i < 2 * n; i++) cin >> arr[i];
+					myQueue = CreateQueue(Element{ arr[0],arr[1] });
+					for (int i = 2; i < 2 * n; i = i + 2) {
+						try {
+							myQueue = Enqueue(myQueue, Element{ arr[i], arr[i + 1] });
+						}
+						catch (const char* msg) {
+							cout << msg << endl;
+							myQueue = NULL;
+							system("pause");
+						}
+					}
+					break;
+				}
+			}
+		} while (variant != 5 && myQueue != NULL || variant != 2 && myQueue == NULL);
+	}
+
+	static int ConsoleInterfaceInput(ConsoleInterface::ConsoleInputBBB putin) {
+		int variant;
+		cin >> variant;
+		if (putin == Creating) {
+			while (variant != 1 && variant != 2) {
+				cout << "Wrong input. Try again > ";
+				cin >> variant;
+			}
+		}
+		else if (putin == Acting) {
+			while (variant != 1 && variant != 2 && variant != 3 && variant != 4 && variant != 5) {
+				cout << "Wrong input. Try again > ";
+				cin >> variant;
+			}
+		}
+		return variant;
+	}
 
 private:
 	// disallow creating an instance of this object
@@ -99,9 +241,5 @@ private:
 
 int main()
 {
-	Queue* myQueue = CreateQueue(Element{ 1,2 });
-	myQueue = Enqueue(myQueue, Element{ 1,4 });
-	myQueue = Enqueue(myQueue, Element{ 1,3 });
-	ConsoleInterface::OutputQueue(myQueue);
-	DeleteQueue(myQueue);
+	ConsoleInterface::MainDialogue();
 }
